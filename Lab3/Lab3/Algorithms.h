cf6db77cd9f667dc0afe87d4da8322cb5e607172
SamvelNalchajyan <nalchajyan.samvel@gmail.com>
2021-05-21 17:46:55 +0300

    Adaptive Simpson

--- Lab3/Lab3/Algorithms.h
+++ Lab3/Lab3/Algorithms.h
@@ -1,6 +1,8 @@
 #pragma once
 #define _USE_MATH_DEFINES
 #include <cmath>
+#include <vector>
+#define MyPi 3.14159265358979
 
 double function_f_x(double x)
 {
@@ -169,4 +171,58 @@ double Adaptive_Rectangle(double a, double b, int id, double eps)
 		I = Adaptive_Rectangle(a, mid, id, eps / 2.0) + Adaptive_Rectangle(mid, b, id, eps / 2.0);
 	}
 	return I;
+}
+
+double FRand(double min, double max)
+{
+	double tmp = (double)(rand()) / RAND_MAX;
+	return min + tmp * (max - min);
+}
+
+double function_dg(double x, double t)
+{
+	std::vector<double> A;
+	std::vector<double> B;
+	for (int i = 0; i < 14; i++)
+	{
+		A.push_back(FRand(-1, 1));
+		B.push_back(FRand(-1, 1));
+	}
+	double alpha = FRand(0, 1);
+	
+
+	double dg = 0;
+	for (int i = 0; i < 14; i++)
+	{
+		dg += (A[i] * sin(2 * MyPi * (i + 1) * (alpha - x) * t) + B[i] * cos(2 * MyPi * (i + 1) * (alpha - x) * t));
+	}
+	return dg;
+}
+
+double Simpson_for_task_2(double x, double a, double b)
+{
+	double I = 0;
+	double h = (b - a) / 2.0;
+	I = (h / 3.0) * (function_dg(x, a) + 4 * function_dg(x, a + h) + function_dg(x, b));
+	return I;
+}
+
+double Adaptive_Simpson_for_task_2(double x, double a, double b, double eps)
+{
+	double mid = (a + b) / 2.0;
+	double I = Simpson_for_task_2(x, a, b);
+	double I_left = Simpson_for_task_2(x, a, mid);
+	double I_right = Simpson_for_task_2(x, mid, b);
+	if (fabs(I_left + I_right - I) >= eps)
+	{
+		I = Adaptive_Simpson_for_task_2(x, a, mid, eps / 2.0) + Adaptive_Simpson_for_task_2(x, mid, b, eps / 2.0);
+	}
+	return I;
+}
+
+double function_g(double x, double eps)
+{
+	double res = Adaptive_Simpson_for_task_2(x, -MyPi / 2.0, MyPi / 2.0, eps);
+	//double res = Simpson_for_task_2(x, -MyPi / 2.0, MyPi / 2.0);
+	return res;
 }
--- Lab3/Lab3/Main.cpp
+++ Lab3/Lab3/Main.cpp
@@ -31,6 +31,22 @@ int main()
 	std::cout << "Eps = 0.000001: " << Adaptive_Rectangle(a, b, 0, eps) << std::endl;
 	std::cout << std::endl;
 	std::cout << "Ответ: 4.38629436" << std::endl;
+	//------------------------------------------------------------------------------//
+	//std::cout << "Функция dg:" << std::endl;
+	//double tmp = MyPi / 3.0;
+	//std::cout << function_dg(0.5, tmp) << std::endl;
+	
+	double eps2 = 4;
+	std::cout << "Функция g:" << std::endl;
+	double h = 0.1;
+	double x = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << function_g(x, eps2) << "  ";
+		x += h;
+	}
+	std::cout << std::endl;
+	
 
 	system("pause");
 	return 0;
